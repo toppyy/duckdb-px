2f4c8c84b080feb91e1c652c9a0ff3d814b14b5f
toppyy <43851547+toppyy@users.noreply.github.com>
2025-10-09 19:54:03 +0300

    large test data

--- test/init_test_large_data.sh
+++ test/init_test_large_data.sh
@@ -1,4 +1,6 @@
 #!bin/bash
 
-wget https://pxweb2.stat.fi/database/StatFin/tyonv/statfin_tyonv_pxt_12ts.px         -O ./test_large/statfin_tyonv_pxt_12ts.px
+mkdir -p ./test/data/large
+
+wget https://pxweb2.stat.fi/database/StatFin/tyonv/statfin_tyonv_pxt_12ts.px         -O ./test/data/large/statfin_tyonv_pxt_12ts.px
 
