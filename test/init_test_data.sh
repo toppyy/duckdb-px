03496506315f09c1c5377aac7d466cc86aa90ed7
toby <43851547+toppyy@users.noreply.github.com>
2025-12-10 19:19:38 +0200

    add tests

--- test/init_test_data.sh
+++ test/init_test_data.sh
@@ -2,10 +2,12 @@
 
 wget https://pxweb2.stat.fi/database/StatFin/vaerak/statfin_vaerak_pxt_11rc.px          -O ./test/data/statfin_vaerak_pxt_11rc.px
 wget https://pxweb2.stat.fi/database/StatFin_Passiivi/akay/statfinpas_akay_pxt_001.px   -O ./test/data/statfin_akay_pxt_001.px
+wget https://pxweb2.stat.fi/database/Statfin/khi/statfin_khi_pxt_11xb.px                -O ./test/data/statfin_khi_pxt_11xb.px
 
 
 curl -X POST https://api.scb.se/OV0104/v1/doris/en/ssd/START/BE/BE0101/BE0101A/BefolkningR1860N \
     -H "Content-Type: application/json" \
     -d @./test/data/query-BefolkningR1860N.json \
     -o "./test/data/SCB-BefolkningR1860N.px" 
+
 
